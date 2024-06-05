05c8e09f128e8a64f76556a89f3d6720472d875d
Fat Sijarina <fs51701@ubt-uni.net>
2024-06-05 09:13:36 +0200

    Base structure

--- ISAS_Project/ISAS_Project/obj/Debug/net8.0/ISAS_Project.AssemblyInfo.cs
+++ ISAS_Project/ISAS_Project/obj/Debug/net8.0/ISAS_Project.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ISAS_Project")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d479df027bb40f1b3467dde74efcf05ba85502a1")]
 [assembly: System.Reflection.AssemblyProductAttribute("ISAS_Project")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ISAS_Project")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
